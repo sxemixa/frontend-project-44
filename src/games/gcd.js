8e804304568671d90e4ed582e88248b1bcb2f97d
sxemixa <sxemixa@gmail.com>
2023-09-16 19:57:30 +0300

    make linter fix

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,29 +1,29 @@
 import { getRandom, basicOfGames } from '../index.js';
 
 export const gcdGame = () => {
-    const purposeOfGCDGame = 'Find the greatest common divisor of given numbers.';
-    const taskForGCDGame = () => {
-        const randomNumber1 = getRandom(90);
-        const randomNumber2 = getRandom(90);
-        const dividers = [];
-        const finalDividers = [];
-        const question = `${randomNumber1} ${randomNumber2}`;        
-        // взять числа от 1 до минимального
-        for (let index = 1; index <= randomNumber1; index++) {
-            // берем только те числа, которые дают нам остаток 0
-            if (randomNumber1 % index === 0) {
-                // эти числа добавляем в массив
-                dividers.push(index);
-            }
-        }
-        for (let index = 0; index < dividers.length; index++) {
-            const element = dividers[index];
-
-            if (randomNumber2 % element === 0) {
-                finalDividers.push(element);
-            }
-        }
-    return [question,Math.max(...finalDividers).toString()];
+  const purposeOfGCDGame = 'Find the greatest common divisor of given numbers.';
+  const taskForGCDGame = () => {
+    const randomNumber1 = getRandom(90);
+    const randomNumber2 = getRandom(90);
+    const dividers = [];
+    const finalDividers = [];
+    const question = `${randomNumber1} ${randomNumber2}`;
+    // взять числа от 1 до минимального
+    for (let index = 1; index <= randomNumber1; index++) {
+      // берем только те числа, которые дают нам остаток 0
+      if (randomNumber1 % index === 0) {
+        // эти числа добавляем в массив
+        dividers.push(index);
+      }
     }
-basicOfGames(purposeOfGCDGame, taskForGCDGame);
+    for (let index = 0; index < dividers.length; index++) {
+      const element = dividers[index];
+
+      if (randomNumber2 % element === 0) {
+        finalDividers.push(element);
+      }
+    }
+    return [question, Math.max(...finalDividers).toString()];
+  };
+  basicOfGames(purposeOfGCDGame, taskForGCDGame);
 };
--- src/games/progression.js
+++ src/games/progression.js
@@ -1,32 +1,30 @@
 import { getRandom, basicOfGames, getRandomindex } from '../index.js';
 
 export const progressionGame = () => {
-    const purposeOfREGGame = 'What number is missing in the progression?';
-    const taskForREGGame = () => {
-        // задаем размер массива
-        const arrayLength = getRandomindex(5, 10);
-        const array = [];
-        // создаем стартовую точку и число на которое
-        // оно будет увеличиваться
-        const firstNumber = getRandom(14);
-        const increase = getRandom(10);
-        // формируем массив размером от 5, до 10
-        for (let index = 0; index < arrayLength; index++) {
-            // пушим в массив числа, начиная с firstNumber и увеличивая
-            // следующее число на insrease
-            for (let num = firstNumber; array.length < arrayLength; num += increase) {
-                array.push(num);
-            }
-        };
-        // теперь берем массив и заменяем рандомное число в нем на '..'
-        const removeIndex = getRandomindex(0, array.length - 1);
-        const finalArr = [...array];
-        finalArr.splice(removeIndex, 1, '..');
-        const question = finalArr.join(' ');
+  const purposeOfREGGame = 'What number is missing in the progression?';
+  const taskForREGGame = () => {
+    // задаем размер массива
+    const arrayLength = getRandomindex(5, 10);
+    const array = [];
+    // создаем стартовую точку и число на которое
+    // оно будет увеличиваться
+    const firstNumber = getRandom(14);
+    const increase = getRandom(10);
+    // формируем массив размером от 5, до 10
+    for (let index = 0; index < arrayLength; index++) {
+      // пушим в массив числа, начиная с firstNumber и увеличивая
+      // следующее число на insrease
+      for (let num = firstNumber; array.length < arrayLength; num += increase) {
+        array.push(num);
+      }
+    }
+    // теперь берем массив и заменяем рандомное число в нем на '..'
+    const removeIndex = getRandomindex(0, array.length - 1);
+    const finalArr = [...array];
+    finalArr.splice(removeIndex, 1, '..');
+    const question = finalArr.join(' ');
 
-
-
-        return [question, array[removeIndex].toString()];
-    };
-basicOfGames(purposeOfREGGame, taskForREGGame);
+    return [question, array[removeIndex].toString()];
+  };
+  basicOfGames(purposeOfREGGame, taskForREGGame);
 };
